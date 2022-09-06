150408bd8fd30d9945788d8bfed369e7e503b582
Elamaran <elamaranp2000@gmail.com>
2022-09-06 18:05:11 +0530

    Updated

--- sort.java
+++ sort.java
@@ -7,7 +7,7 @@ class Sort {
         for (int i=0; i<n-1; i++){
             if (arr[i] > arr[i+1]) {
                 int temp = arr[i];
-                arr[i] = arr[i+1];
+                arr[i] = arr[i+1]; //Assigning
                 arr[i+1] = temp;  //act
             }
         }
