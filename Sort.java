a7e8c8f0f6f9b26a4e6b26125a514ae866a08088
Elamaran <elamaranp2000@gmail.com>
2022-09-08 12:37:58 +0530

    Changes updates

--- Sort.java
+++ Sort.java
@@ -1,27 +1,51 @@
-class Sort {
-    static void bubbleSort(int arrayToBeSorted[], int n){
-        if (n == 1){
-            return;                  //return if we encounter n == 1
-        }
-        for (int i=0; i<n-1; i++){
-            if (arrayToBeSorted[i] > arrayToBeSorted[i+1]) {
-                int temp = arrayToBeSorted[i];
-                arrayToBeSorted[i] = arrayToBeSorted[i+1]; //Assigning
-                arrayToBeSorted[i+1] = temp;
-            }
-        }
-        bubbleSort(arrayToBeSorted, n-1);
+
+import java.util.stream.IntStream;
+
+/**
+ * buble sort
+ * @author elamaran
+ */
+
+public class Sort {
+    /**
+     * Traversing the array with i
+     * With each value of i traversing the array once again with j
+     * When arrayToBeSorted[i] > arrayToBeSorted[j+1] it swaps the value
+     * @param arrayToBeSorted
+     * @param noOfElements
+     */
+    public void bubbleSort(int arrayToBeSorted[],int noOfElements) {
+        IntStream.range(0, noOfElements - 1).forEach(i -> {
+            IntStream.range(0, noOfElements - i - 1).forEach(j -> {
+                if (arrayToBeSorted[j] > arrayToBeSorted[j + 1]) {
+                    int temp = arrayToBeSorted[j];
+                    arrayToBeSorted[j] = arrayToBeSorted[j + 1];
+                    arrayToBeSorted[j + 1] = temp;
+                }
+            });
+        });
     }
-    void display(int arrayToBeSorted[]) {  //display the array elements
-        for (int i=0; i<arrayToBeSorted.length; ++i){
-            System.out.print(arrayToBeSorted[i]+" ");
-        }
+    /**
+     * Printing the sorted array
+     * @param arrayToBeSorted
+     * @param noOfElements
+     */
+    public void printArray(int arrayToBeSorted[],int noOfElements) {
+        IntStream.range(0, noOfElements).forEach(i -> {
+            System.out.print(arrayToBeSorted[i] + " ");
+        });
     }
 
-    public static void main(String[] args) {
-        Sort sortObject= new Sort();
-        int arrayToBeSorted[] = {18, 14, 5, 12, 2, 11, 9};
-        bubbleSort(arrayToBeSorted, arrayToBeSorted.length);
-        sortObject.display(arrayToBeSorted);
+    /**
+     * In main function initialising pbject of the class Sort
+     * Initialising a new array arrayToBeSorted
+     * calling the functions bubbleSort and printArray
+     * @param args
+     */
+    public static void main(String args[]) {
+        Sort sortObject = new Sort();
+        int arrayToBeSorted[] = { 64, 34, 25, 12, 22, 11, 90 };
+        sortObject.bubbleSort(arrayToBeSorted,arrayToBeSorted.length);
+        sortObject.printArray(arrayToBeSorted,arrayToBeSorted.length);
     }
 }
