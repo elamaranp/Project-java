de64f7c8ecc1b89fa7db88da1d239e6ec679d42f
Elamaran <elamaranp2000@gmail.com>
2022-09-19 08:44:01 +0530

    RestApi Added3

--- src/main/java/com/example/twitterapi/ApiController/Controller.java
+++ src/main/java/com/example/twitterapi/ApiController/Controller.java
@@ -9,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * RestController Annotation that is itself annotated
+ * with Controller and ResponseBody
+ * Controller controlls all Requests
+ */
 @RestController
 public class Controller {
+    /**
+     * PostTweet method for posting the tweet
+     * When the controller hits a request of /tweet
+     * It Maps with the PostTweet Endpoint and executes the Method
+     * returns whether the call successfull or not
+     */
     @PostMapping("/api/1.0/twitter/tweet")
     public String PostTweet(@RequestBody String msg){
         PostTweet object=new PostTweet();
@@ -18,6 +29,12 @@ public class Controller {
         return result;
     }
 
+    /**
+     *TimeLine Method for getting the details we posted
+     * When the controller hits a request of /timeline
+     * It Maps with the TimeLine Endpoint and executes the Method
+     * returns the result in a list
+     */
     @GetMapping("/api/1.0/twitter/timeline")
     public List<Status> TimeLine(){
         TwitterTimeLine object=new TwitterTimeLine();
