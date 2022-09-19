eb5ce6e83b9966798b29a8684abbde77c8b322fb
Elamaran <elamaranp2000@gmail.com>
2022-09-19 08:37:36 +0530

    RestApi Added2

--- src/main/java/com/example/twitterapi/TwitterApiApplication.java
+++ src/main/java/com/example/twitterapi/TwitterApiApplication.java
@@ -3,6 +3,11 @@ package com.example.twitterapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBootApplication automatically contains Configuration,ComponentScan,EnableAutoConfiguration
+ * Main SrpingBootappilaction for posting tweet
+ * And Getting TimeLine
+ */
 @SpringBootApplication
 public class TwitterApiApplication {
 
