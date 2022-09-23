c54a66dec5080f1c83425b8c88ccf70684966d40
Elamaran <elamaranp2000@gmail.com>
2022-09-23 11:46:19 +0530

    GraphQl added

--- src/main/java/com/example/twitterapi/ApiController/Controller.java
+++ src/main/java/com/example/twitterapi/ApiController/Controller.java
@@ -1,11 +1,13 @@
 package com.example.twitterapi.ApiController;
 import com.example.twitterapi.ApiPostTweet.PostTweet;
 import com.example.twitterapi.ApiGetTimeLine.TwitterTimeLine;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import twitter4j.Status;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,7 +16,7 @@ import java.util.List;
  * with Controller and ResponseBody
  * Controller controlls all Requests
  */
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
     /**
      * PostTweet method for posting the tweet
@@ -22,8 +24,8 @@ public class Controller {
      * It Maps with the PostTweet Endpoint and executes the Method
      * returns whether the call successfull or not
      */
-    @PostMapping("/api/1.0/twitter/tweet")
-    public String PostTweet(@RequestBody String msg){
+    @MutationMapping("tweet")
+    public String PostTweet(@Argument String msg){
         PostTweet object=new PostTweet();
         String result=object.postTweet(msg);
         return result;
@@ -35,7 +37,7 @@ public class Controller {
      * It Maps with the TimeLine Endpoint and executes the Method
      * returns the result in a list
      */
-    @GetMapping("/api/1.0/twitter/timeline")
+    @QueryMapping("timeline")
     public List<Status> TimeLine(){
         TwitterTimeLine object=new TwitterTimeLine();
         List<Status> result=object.returnTimeLine();
